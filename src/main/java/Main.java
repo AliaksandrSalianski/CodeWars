6c3ab5a7a7a4dec0e4d0857f93ffd2315e0de3f5
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-04 11:44:22 -0400

    codwars_level7_Anogramms

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
+import codewars.level7.Anogramms;
 import codewars.level8.CountSheep;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(Anogramms.isAnagram("Twoo", "Woot"));
     }
 
 }
