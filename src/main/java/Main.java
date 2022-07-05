c896a55f4a46d406e3c0fd9192fe87cc9ce209dd
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-05 13:24:19 -0400

    codwars_level7_ArrayOneTwo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
-import codewars.level7.Anogramms;
-import codewars.level8.CountSheep;
+
+import codewars.level7.ArrayOneTwo;
+
+
+import java.util.Arrays;
+
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(Anogramms.isAnagram("Twoo", "Woot"));
-    }
 
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(ArrayOneTwo.alternate(5, "one", "two")));
+        System.out.println(Arrays.toString(ArrayOneTwo.newAlter(6, "A", "B")));
+    }
 }
