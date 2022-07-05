2f7b34115583e4d0d5a02cb3d84226563a9fd14f
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-05 16:08:49 -0400

    codwars_level7_WaterBombs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
 import codewars.level7.PrevMultOfThree;
+import codewars.level7.WaterBombs;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(PrevMultOfThree.prevMultOfThree(25));
+        System.out.println(WaterBombs.waterBombs("xxxxxxxxxxxxxxxxx", 4));
     }
 }
---
+++ src/main/java/codewars/level7/WaterBombs.java
@@ -0,0 +1,39 @@
+package codewars.level7;
+
+import java.util.Locale;
+
+public class WaterBombs {
+    /*
+    Данная строка представляет собой двумерную плоскость произвольной длины, состоящую из двух символов:
+    xпредставляющий огонь
+    Yпредставляющие здания.
+    Вода, которую вы сбрасываете, не может пройти сквозь здания, и поэтому отдельные участки пожара должны рассматриваться отдельно.
+    Ваши водяные бомбы могут тушить только смежные участки огня шириной до ширины (параметр w).
+    Вы должны вернуть минимальное количество водяных бомб, необходимое для тушения пожара в строке.
+    Примечание: все входные данные будут действительными.
+     */
+    public static int waterBombs(String fire, int w) {
+        String fireToWork = fire.toLowerCase();
+        char[] array = fireToWork.toCharArray();
+        int countOfFireX = 0;
+        int bomb = 0;
+        for (char charInFire : array) {
+            if ((charInFire == 'y') &&(countOfFireX > 0)) {
+                countOfFireX = 0;
+                bomb++;
+                continue;
+            }
+            if ((charInFire == 'x') &&(countOfFireX < w)) {
+                countOfFireX++;
+            } else if (countOfFireX == w) {
+                countOfFireX = 1;
+                bomb++;
+            }
+
+
+        }
+        if(countOfFireX>0)
+            bomb++;
+        return  bomb;
+    }
+}
