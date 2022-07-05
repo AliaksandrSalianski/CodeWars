6c8c2b4eb8cc7a1503dd015fdfda49461217b3b9
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-05 14:57:14 -0400

    codwars_level7_PrevMult

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,8 @@
-
-import codewars.level7.ArrayOneTwo;
-
-
-import java.util.Arrays;
-
+import codewars.level7.PrevMultOfThree;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(ArrayOneTwo.alternate(5, "one", "two")));
-        System.out.println(Arrays.toString(ArrayOneTwo.newAlter(6, "A", "B")));
+        System.out.println(PrevMultOfThree.prevMultOfThree(25));
     }
 }
