3a2fb0073af6237dbcfc4e5c575b5613f5de3601
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-05 17:37:24 -0400

    delete if

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
+import codewars.level7.Duplicates;
 import codewars.level7.PrevMultOfThree;
 import codewars.level7.WaterBombs;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(WaterBombs.waterBombs("xxxxxxxxxxxxxxxxx", 4));
+        int [] array = new int[] {11,11,11,11,11};
+        System.out.println(Duplicates.giveDuplicates(array));
     }
 }
