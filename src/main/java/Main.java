6c66519225debf04d8ab3fc7e0191bf40562389b
AliaksandrSalianski <AliaksandrSalianski@gmail.com>
2022-07-08 14:53:04 -0400

    codewars_level7_MinimmumInArray: -fist method not work(comment)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import codewars.level7.MinimumInArray;
 public class Main {
 
     public static void main(String[] args) {
-        int[] array = new int[]{22,3,2,1,1,1,12,5,6,7,1};
-        System.out.println(MinimumInArray.minValue(array));
+
     }
 }
--- src/main/java/codewars/level7/MinimumInArray.java
+++ src/main/java/codewars/level7/MinimumInArray.java
@@ -1,37 +1,53 @@
 package codewars.level7;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class MinimumInArray {
+    // Учитывая список цифр , верните наименьшее число , которое можно составить из этих цифр, используя цифры только один раз (игнорируя дубликаты).
+        ///not passed the tests in CodeWars
 
-    public static int minValue(int[] values) {
-        Arrays.sort(values);
-        int sizeTheResultArray = 0;
-        for (int i = 1; i < values.length; i++) {
-            if (values[i] == values[i - 1] || values[i] > 9) {
-                sizeTheResultArray++;
-            }
-        }
-        int[] resultArray = new int[values.length - sizeTheResultArray];
-        int indexInResultArray = 0;
-        if (resultArray.length > 1) {
-            resultArray[indexInResultArray] = values[0];
-            indexInResultArray++;
-        }
-        Arrays.sort(resultArray);
-        int helpToChangeIndex = resultArray[resultArray.length - 1];
+  //      Arrays.sort(values);
+  //      int minusInSizeForResultArray = 0;
+  //      for (int i = 1; i < values.length; i++) {
+  //          if (values[i] == values[i - 1] || values[i] > 9) {
+  //              minusInSizeForResultArray++;
+  //          }
+  //      }
+  //      int[] resultArray = new int[values.length - minusInSizeForResultArray];
+  //      int indexInResultArray = 0;
+  //      if (resultArray.length > 1) {
+  //          resultArray[indexInResultArray] = values[0];
+  //          indexInResultArray++;
+  //      }
+  //      Arrays.sort(resultArray);
+  //      int helpToChangeIndex = resultArray[resultArray.length - 1];
+  //      for (int number : values) {
+  //          if ((Arrays.binarySearch(resultArray, number) < 0) && (number <= 9)) {
+  //              resultArray[indexInResultArray] = number;
+  //              indexInResultArray++;
+  //              if (indexInResultArray >= resultArray.length) {
+  //                  break;
+  //              }
+  //          }
+  //      }
+  //      resultArray[0] = helpToChangeIndex;
+  //      int result = 0;
+  //      for (int number : resultArray) {
+  //          result = result * 10 + number;
+  //      }
+  //      return result;
+  //  }
+        /// This method is working in CodeWars
+    public static int min(int[] values) {
+        TreeSet<Integer> mySet = new TreeSet<>();
         for (int number : values) {
-            int index = Arrays.binarySearch(resultArray, number);
-            if ((index < 0) && (number <= 9)) {
-                resultArray[indexInResultArray] = number;
-                indexInResultArray++;
+            if (number <= 9) {
+                mySet.add(number);
             }
         }
-        resultArray[0] = helpToChangeIndex;
         int result = 0;
-        for (
-                int number : resultArray) {
-            result = result * 10 + number;
+        for (Integer num : mySet) {
+            result = result * 10 + num;
         }
         return result;
     }
